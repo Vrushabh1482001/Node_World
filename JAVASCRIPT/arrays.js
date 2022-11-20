7349b13156649b063c3f9ddcff630ad30d350483
Vrushabh1482001 <vrushabhsatasiya2001@emai.com>
2022-11-20 20:55:45 +0530

    array

--- JAVASCRIPT/arrays.js
+++ JAVASCRIPT/arrays.js
@@ -1,1 +1,81 @@
-console.log(typeof []);
+// Array
+
+//let arr = [10, 11, 12, 13, 14, 15, 16, 17];
+// arr.length = 100;
+// console.log(typeof arr);
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[5]);
+// console.log(arr["10"]);
+// arr[50] = 550;
+// arr[49] = undefined;
+// arr[3.1] = 15;
+// console.log(arr);
+// let arr = ["ten", 11, "twelve", 13, 14, "15", 16, "17"];
+// console.log(arr);
+// The length property of an Array object represents the number of elements in that array
+// console.log(arr.length);
+// arr.length = 10;
+// console.log(arr);
+// console.log(arr.length);
+// arr[9] = undefined;
+// arr[5] = undefined;
+// arr[5] = 20;
+// arr[3.1] = 3.1;
+// arr["A"] = "a";
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[9]);
+// arr.length = 100;
+// console.log(arr);
+// arr[50] = 50;
+// console.log(arr.length);
+// arr['50'] = "fifty";
+// console.log(arr.length);
+// console.log(arr);
+
+
+// let arr1={"A":"a","B":"b"}
+// let arr11=["A":"a","B":"b"]
+// console.log(arr1);
+
+// console.log(arr_new[-2])
+// arr_new.push(18);
+// arr_new.pop();
+// arr_new.pop();
+// arr_new.pop();
+// console.log(arr_new)
+// arr_new.shift();
+// arr_new.shift();
+// arr_new.shift();
+// arr_new.unshift(9);
+// console.log(arr_new)
+// console.log(arr_new)
+// console.log(arr_new)
+// arr_new.splice(5);
+// arr_new.splice(5,1);
+// arr_new.splice(-4,2);
+// arr_new.splice(-4,-2);
+// arr_new.splice(4,-2);
+// arr_new.splice(-3);
+// arr_new.splice(0,2,30,31);
+// arr_new.splice(0,2,30,31,32,33);
+// arr_new.splice(0,arr_new.length,30,31,32,33);
+// arr_new.splice(0,0,30,31,32,33);
+// arr_new.splice(5,30,31,32,33);
+// arr_new.splice(0,5,30,31,32,33);
+// arr_new.splice(5,0,30,31,32,33);
+
+
+// console.log(arr_new.splice(2));
+
+// let arr_new=[10,11,12,13,14,15,16,17,18,19,20];                                
+// console.log(arr_new.splice(3));
+// console.log(arr_new.splice(3,3));
+// console.log(arr_new.splice(3,100,21,22,23));
+
+
+
+
+
+
