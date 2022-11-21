880a4cbfd984c86b114403ef3706020d75839d92
Vrushabh1482001 <vrushabhsatasiya2001@emai.com>
2022-11-21 11:20:20 +0530

    array changes

--- JAVASCRIPT/arrays.js
+++ JAVASCRIPT/arrays.js
@@ -1,6 +1,6 @@
 // Array
 
-//let arr = [10, 11, 12, 13, 14, 15, 16, 17];
+// let arr = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 // arr.length = 100;
 // console.log(typeof arr);
 // console.log(arr);
@@ -9,13 +9,15 @@
 // console.log(arr["10"]);
 // arr[50] = 550;
 // arr[49] = undefined;
-// arr[3.1] = 15;
+// arr[3.1] = 60;
 // console.log(arr);
+
+
 // let arr = ["ten", 11, "twelve", 13, 14, "15", 16, "17"];
 // console.log(arr);
 // The length property of an Array object represents the number of elements in that array
 // console.log(arr.length);
-// arr.length = 10;
+// arr.length = 100;
 // console.log(arr);
 // console.log(arr.length);
 // arr[9] = undefined;
@@ -38,41 +40,45 @@
 // let arr1={"A":"a","B":"b"}
 // let arr11=["A":"a","B":"b"]
 // console.log(arr1);
+// console.log(arr_new[-2]);
 
-// console.log(arr_new[-2])
-// arr_new.push(18);
-// arr_new.pop();
-// arr_new.pop();
-// arr_new.pop();
+// let arr_new = [10, 11, 12, 13, 14, 15, 16, 17, 18];
+// arr_new.length = 100;
+// console.log(arr_new.length);
+// arr_new.push(19);  // Last insert
+// arr_new.pop();     // Last delete
 // console.log(arr_new)
-// arr_new.shift();
-// arr_new.shift();
-// arr_new.shift();
-// arr_new.unshift(9);
+
+// arr_new.shift();   // First delete
+// arr_new.unshift(10);  // First insert
 // console.log(arr_new)
-// console.log(arr_new)
-// console.log(arr_new)
+
+// splice 
 // arr_new.splice(5);
-// arr_new.splice(5,1);
-// arr_new.splice(-4,2);
-// arr_new.splice(-4,-2);
-// arr_new.splice(4,-2);
+// arr_new.splice(5, 1);
+// arr_new.splice(-4, 2);
+// arr_new.splice(-4, -2);
+// arr_new.splice(4, -2);
 // arr_new.splice(-3);
-// arr_new.splice(0,2,30,31);
-// arr_new.splice(0,2,30,31,32,33);
-// arr_new.splice(0,arr_new.length,30,31,32,33);
-// arr_new.splice(0,0,30,31,32,33);
+// arr_new.splice(5, 2, 30, 31);
+// arr_new.splice(5, 2, 30, 31, 32, 33);
+// arr_new.splice(0, arr_new.length, 30, 31, 32, 33);
+// arr_new.splice(0, 0, 30, 31, 32, 33);
 // arr_new.splice(5,30,31,32,33);
 // arr_new.splice(0,5,30,31,32,33);
 // arr_new.splice(5,0,30,31,32,33);
+// console.log(arr_new);
+// console.log(arr_new.length);
 
 
 // console.log(arr_new.splice(2));
 
-// let arr_new=[10,11,12,13,14,15,16,17,18,19,20];                                
+// let arr_new = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 // console.log(arr_new.splice(3));
-// console.log(arr_new.splice(3,3));
-// console.log(arr_new.splice(3,100,21,22,23));
+// console.log(arr_new.splice(3, 3));
+// console.log(arr_new.splice(3, 3, 21, 22, 23));
+
+
 
 
 
