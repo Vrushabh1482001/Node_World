26c1d39f45384dfd11a7ccee251a2aec490e0521
Vrushabh1482001 <vrushabhsatasiya2001@emai.com>
2022-11-28 11:06:16 +0530

    math and strfunction

---
+++ JAVASCRIPT/Prompt/Function/arrayCopywithin.js
@@ -0,0 +1,15 @@
+
+// console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].copyWithin(-3));
+// console.log([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].copyWithin(2, 4, 6));
+// [1, 2, 3, 3, 4]
+
+// console.log(Math.ceil(36.50));
+
+// const arr = [1, 2, 3];
+// const max = arr.reduce((a, b) => Math.max(a, b), -Infinity);
+
+
+// console.log(Math.random() * 10);
+const worldString = "Hello, world";
+console.log(worldString.small()); // <small>Hello, world</small>
+console.log(worldString.big()); 
