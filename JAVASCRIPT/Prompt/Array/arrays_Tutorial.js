cb069e8e4ba1b9659d2cc998ba37de9c60bab1ec
Vrushabh1482001 <vrushabhsatasiya2001@emai.com>
2022-12-06 17:50:53 +0530

    arr

--- JAVASCRIPT/Prompt/Array/arrays_Tutorial.js
+++ JAVASCRIPT/Prompt/Array/arrays_Tutorial.js
@@ -151,6 +151,56 @@ where start and end represent the index of items in that array.The original arra
 // console.log(typeof arr_new.join());
 
 
+// 6/12/2022 -----------------------------------------------------------------------------
+
+// The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+
+// const numbers = [1, 2, 3, 4];
+// const doubled = numbers.map(item => item * 2);
+// console.log(doubled)
+
+
+// The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
+// console.log(
+//     [1, 2, 3, 4, 5].filter((element) => {
+//         return element >= 3;
+//     })
+// ); //[ 3, 4, 5 ]
+
+// const numbers = [1, 2, 3, 4];
+// const evens = numbers.filter(item => item % 2 === 0);
+// console.log(evens);
+
+
+// const students = [
+//     { name: 'Quincy', grade: 96 },
+//     { name: 'Jason', grade: 84 },
+//     { name: 'Alexis', grade: 100 },
+//     { name: 'Sam', grade: 65 },
+//     { name: 'Katie', grade: 90 }
+//   ];
+  
+//   const studentGrades = students.filter(student => student.grade >= 90);
+//   console.log( studentGrades); 
+
+
+
+// console.log(
+//     [1, 2, 3, 4, 5].map((element, index) => {
+//         return element * 100;
+//     }).filter((element, index) => {
+//         return element >= 300;
+//     })
+// );
+
+// console.log(
+//     [1, 2, 3, 4, 5].reduce((firstElement, nextElement) => {
+//         console.log(firstElement +" - " + nextElement)
+//         return firstElement + nextElement;
+//     },10)
+// );
+
+
 
 
 
