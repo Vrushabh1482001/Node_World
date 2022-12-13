3964c50687e7fb251110341d2228f3e3d7cca5e2
Vrushabh1482001 <vrushabhsatasiya2001@emai.com>
2022-12-13 20:47:22 +0530

    json

---
+++ HTML/event/JavascriptObject/object.js
@@ -0,0 +1,30 @@
+let student0 = {
+    Name: "Satasiya Vrushabh",
+    Age: 22,
+    Course: "Node Js",
+    MobileNo: 7069182001
+}
+let student1 = {
+    Name: "Keyur Padmani",
+    Age: 22,
+    Course: "Flutter",
+    MobileNo: 8877665544
+}
+let student2 = {
+    Name: "Krunal Sojitra",
+    Age: 21,
+    Course: "c++",
+    MobileNo: 7069182001
+}
+let student3 = {
+    Name: "Neel",
+    Age: 22,
+    Course: "Python",
+    MobileNo: 9974099876
+}
+let student4 = {
+    Name: "Keval",
+    Age: 26,
+    Course: "Data Structure",
+    MobileNo: 9909045678
+}
