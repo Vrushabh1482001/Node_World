f8b53d30e121d903ef1d3e821d5737f7161a59b2
Vrushabh1482001 <vrushabhsatasiya2001@emai.com>
2023-01-03 16:36:14 +0530

    login

---
+++ NODEJS/Express/login/login.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+login = express.Router();
+login.use(express.json());
+
+
+login.get("/", (req, res) => {
+    res.json({ 'msg': "inside a login" })
+});
+
+// login.get("/singup", (req, res) => {
+//     res.sendFile(__dirname + "/login.html");
+// });
+
+login.post("/singup", (req, res) => {
+    const request = req.body;
+    let id = request.id;
+    let pwd = request.pwd;
+    console.log(id);
+    console.log(pwd);
+    if (id == pwd) {
+        const dashboard = require("./dashboard");
+        login.use("/dashboard", dashboard);
+    }
+    else {
+        const reset = require("./reset");
+        login.use("/reset", reset);
+    }
+});
+
+module.exports = login;
+
